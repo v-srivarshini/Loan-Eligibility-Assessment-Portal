f2bc037c99b82a55b3b88ab6230e1db8bd18dbd6
Sri Varshini Vuchuru <srivarshinivuchuru@gmail.com>
2026-06-27 21:42:45 +0530

    Updated db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.error("MongoDB Error:", error);
     process.exit(1);
   }
 };
