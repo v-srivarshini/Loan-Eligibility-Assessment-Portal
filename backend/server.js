fe1c3218fbd071c037a0460b8fca59a78c7158c9
Sri Varshini Vuchuru <srivarshinivuchuru@gmail.com>
2026-06-27 19:01:29 +0530

    Fix CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:"https://loan-eligibility-assessment-portal.vercel.app",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api", applicantRoutes);
 
