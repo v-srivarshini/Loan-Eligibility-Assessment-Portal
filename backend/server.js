1d0e31d6fda23a6d944f46397207a701c1064bc0
Sri Varshini Vuchuru <srivarshinivuchuru@gmail.com>
2026-06-27 21:20:09 +0530

    changing cord

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin:"https://loan-eligibility-assessment-portal.vercel.app",
-  methods: ["GET", "POST"],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use("/api", applicantRoutes);
 
